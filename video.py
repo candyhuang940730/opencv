61b82c07b6a5aae7ceaeb9c8e2b58dbaf4ec6f39
xin345215 <47916505+candyhuang940730@users.noreply.github.com>
2026-06-05 14:36:59 +0800

    video practice

--- video.py
+++ video.py
@@ -1,1 +1,18 @@
 import cv2
+
+cap = cv2.VideoCapture(1)
+
+# 使用迴圈取得下一幀圖片
+while True:
+    ret, frame = cap.read()
+    if ret:
+        frame = cv2.resize(frame, (0, 0), fx=1.2, fy=1.2)
+        cv2.imshow('video', frame)
+    else:
+        break
+    # 影片速度調整
+    # cv2.waitKey(1)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+        # 按Q可結束影片
